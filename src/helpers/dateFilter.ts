f7aaae3ac6de2e05a63b8909d96daa10f6a82f25
LeoRangel <leandro.mdrs06@gmail.com>
2021-10-15 11:54:22 -0300

    Update 'dateFilter'

--- src/helpers/dateFilter.ts
+++ src/helpers/dateFilter.ts
@@ -22,3 +22,21 @@ export const filterListByMonth = (list: Item[], date: string): Item[] => {
 
     return newList
 }
+
+export const formatDate = (date: Date): string => {
+    let year = date.getFullYear();
+    let month = date.getMonth() + 1;
+    let day = date.getDate();
+
+    return `${addZeroToDate(day)}/${addZeroToDate(month)}/${year}`;
+}
+// Sample: day 6 -> day 06
+const addZeroToDate = (n: number): string => {
+    return n < 10 ? `0${n}` : `${n}`;
+}
+
+export const formatCurrentMonth = (currentMonth: string): string => {
+    let [year, month] = currentMonth.split('-');
+    let months = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'];
+    return `${months[parseInt(month) - 1]} de ${year}`;
+}
