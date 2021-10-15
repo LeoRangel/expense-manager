74a272ce0485d8a175ab97cacba7b368ad59129c
LeoRangel <leandro.mdrs06@gmail.com>
2021-10-15 15:37:36 -0300

    Create 'InfoArea' and 'ResumeItem' components + Show info area

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,43 @@ import * as C from './App.styles'
 // Data
 import { Item } from './types/Item';
 // import { Category } from './types/Category';
-// import { categories } from './data/categories';
+import { categories } from './data/categories';
 import { items } from './data/items';
 import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter';
 // Components
 import { TableArea } from './components/TableArea';
+import { InfoArea } from './components/InfoArea';
 
 const App = () => {
   const [list, setList] = useState(items);
   const [filteredList, setFilteredList] = useState<Item[]>([]);
   const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  const [income, setIncome] = useState(0);
+  const [expense, setExpense] = useState(0);
 
   useEffect(() => {
     setFilteredList(filterListByMonth(list, currentMonth))
   }, [list, currentMonth])
+
+  useEffect(() => {
+    let incomeCount = 0;
+    let expenseCount = 0;
+
+    for (let i in filteredList) {
+      if (categories[filteredList[i].category].expense) {
+        expenseCount += filteredList[i].value;
+      } else {
+        incomeCount += filteredList[i].value;
+      }
+    }
+
+    setIncome(incomeCount);
+    setExpense(expenseCount);
+  }, [filteredList]);
+
+  const handleMonthChange = (newMonth: string) => {
+    setCurrentMonth(newMonth);
+  }
 
   return (
     <C.Container>
@@ -25,7 +48,12 @@ const App = () => {
       </C.Header>
       <C.Body>
 
-        {/* InfoArea */}
+        <InfoArea
+          currentMonth={currentMonth}
+          onMonthChange={handleMonthChange}
+          income={income}
+          expense={expense}
+        />
 
         {/* InputArea */}
 
