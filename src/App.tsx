b90cde5921037d739896b1bd02c46e96b618a234
LeoRangel <leandro.mdrs06@gmail.com>
2021-10-15 12:02:30 -0300

    Create 'TableArea' and 'TableItem' components + Show items

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
+import { useEffect, useState } from 'react';
 import * as C from './App.styles'
+// Data
+import { Item } from './types/Item';
+// import { Category } from './types/Category';
+// import { categories } from './data/categories';
+import { items } from './data/items';
+import { filterListByMonth, getCurrentMonth } from './helpers/dateFilter';
+// Components
+import { TableArea } from './components/TableArea';
 
-function App() {
+const App = () => {
+  const [list, setList] = useState(items);
+  const [filteredList, setFilteredList] = useState<Item[]>([]);
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+
+  useEffect(() => {
+    setFilteredList(filterListByMonth(list, currentMonth))
+  }, [list, currentMonth])
+
   return (
     <C.Container>
       <C.Header>
@@ -12,7 +29,7 @@ function App() {
 
         {/* InputArea */}
 
-        {/* TableArea */}
+        <TableArea list={filteredList} />
 
       </C.Body>
     </C.Container>
