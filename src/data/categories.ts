c4fa40949cd4a75b0abda0ede4e2f1819616f8ab
LeoRangel <leandro.mdrs06@gmail.com>
2021-11-01 18:48:51 -0300

    Add static test data

--- src/data/categories.ts
+++ src/data/categories.ts
@@ -1,7 +1,10 @@
 import { Category } from '../types/Category';
 
 export const categories: Category = {
-    food: { title: 'Alimentação', color: 'blue', expense: true },
-    rent: { title: 'Aluguel', color: 'brown', expense: true },
-    salary: { title: 'Salário', color: 'green', expense: false }
+    food: { title: 'Alimentação', color: '#F39237', expense: true },
+    rent: { title: 'Aluguel', color: '#46237A', expense: true },
+    other: { title: 'Outros', color: 'gray', expense: true },
+    clothe: { title: 'Roupas', color: '#2176AE', expense: true },
+    pix: { title: 'Pix', color: '#0EAA4A', expense: false },
+    salary: { title: 'Salário', color: '#0EAA4A', expense: false }
 }
--- src/data/items.ts
+++ src/data/items.ts
@@ -1,8 +1,25 @@
 import { Item } from '../types/Item';
 
 export const items: Item[] = [
-    { date: new Date(2021, 9, 6), category: 'food', title: 'McDonalds', value: 32.12 },
-    { date: new Date(2021, 9, 15), category: 'food', title: 'Burger King', value: 28 },
-    { date: new Date(2021, 9, 16), category: 'rent', title: 'Aluguel Apt', value: 2300 },
-    { date: new Date(2021, 10, 18), category: 'salary', title: 'Salário ACME', value: 4500 },
+    { date: new Date(2021, 8, 12), category: 'food', title: 'Lanche', value: 10 },
+    { date: new Date(2021, 8, 20), category: 'salary', title: 'Salário', value: 3200 },
+    { date: new Date(2021, 8, 25), category: 'rent', title: 'Aluguel', value: 1500 },
+
+    { date: new Date(2021, 9, 6), category: 'clothe', title: 'Calça', value: 90 },
+    { date: new Date(2021, 9, 20), category: 'salary', title: 'Salário', value: 3200 },
+    { date: new Date(2021, 9, 21), category: 'food', title: 'Lanche', value: 15 },
+    { date: new Date(2021, 9, 25), category: 'rent', title: 'Aluguel', value: 1500 },
+
+    { date: new Date(2021, 10, 5), category: 'food', title: 'Lanche', value: 16 },
+    { date: new Date(2021, 10, 20), category: 'salary', title: 'Salário', value: 3200 },
+    { date: new Date(2021, 10, 25), category: 'rent', title: 'Aluguel', value: 1500 },
+    { date: new Date(2021, 10, 25), category: 'other', title: 'Notebook novo', value: 3500 },
+
+    { date: new Date(2021, 11, 7), category: 'food', title: 'Hamburguer', value: 35 },
+    { date: new Date(2021, 11, 16), category: 'clothe', title: 'Camisa', value: 120 },
+    { date: new Date(2021, 11, 18), category: 'other', title: 'Fone', value: 180 },
+    { date: new Date(2021, 11, 20), category: 'salary', title: 'Salário', value: 3200 },
+    { date: new Date(2021, 11, 25), category: 'rent', title: 'Aluguel', value: 1500 },
+    { date: new Date(2021, 11, 28), category: 'food', title: 'Lanche', value: 16 },
+    { date: new Date(2021, 11, 28), category: 'pix', title: 'Pix recebido', value: 100 },
 ];
